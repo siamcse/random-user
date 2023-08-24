c4f1e6ac0a94c7f41ad1053fc69b1bd9ab23d45a
Abu Siam <siamcse01@gmail.com>
2023-08-24 21:11:16 +0600

    random-user setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=''>
+            <div className='bg-gray-800 h-[443px]'>
+                <div className='flex items-center justify-center pt-12'>
+                    <h1 className='uppercase text-5xl text-white'>Random User Generator</h1>
+                    <p>A free, open-source API for generating random user data. Like Lorem Ipsum, but for people.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
